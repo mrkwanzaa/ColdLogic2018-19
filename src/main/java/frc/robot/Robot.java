1d86ff8c02dbf74b2bad96ee7b99a6289d15b18b
Programmer <coldlogic3617@gmail.com>
2019-01-09 19:20:37 -0500

    Why did I leave that comment

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -87,7 +87,6 @@ private AnalogInput ultrasonic = new AnalogInput(ultraPort);
   public void teleopPeriodic() {
       stickY = lstick.getY();
       stickX = lstick.getX();
-//remove git test push
       
       //Right side
       robo.getController(0).set(stickY+stickX);
